55866477d1727f188ab9086d0ad0c78fa8aad354
kygoh <gohkengyew@gmail.com>
2023-09-27 12:47:22 +0000

    test ion-datetime on @ionic/angular@7.4.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import type { DatetimeChangeEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  maxDate = '';
+  value = '';
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit() {
+    const OFFSET = 100;
+    const today = new Date();
+    const futureDate = new Date(today.getFullYear() + OFFSET, 12);
+    this.maxDate = futureDate.toISOString().substring(0, 10);
+  }
+
+  onDatetimeChange($event: Event) {
+  }
 
 }
